a76f0791d6aaadf9db6d3dcdffaa986ad1c0bf5e
Mahesh Gupta <mahesh@market-pulse.in>
2019-04-13 16:40:10 +0530

    Refactoring

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,34 +1,39 @@
 require_relative '../src/item'
 require_relative '../src/item_type'
-require_relative '../src/item_quality'
 
 RSpec.describe Item do
 
   it 'should update sell in when item is updated' do
-    item = Item.new(200, ItemQuality.new(50), ItemType::NORMAL)
+    item = Item.new(200, 50, ItemType::NORMAL)
     item.update
     expect(item.sell_in).to eq(199)
   end
 
   it 'should not update sell_in when item is legendary' do
-    item = Item.new(200, ItemQuality.new(50), ItemType::LEGENDARY)
+    item = Item.new(200, 50, ItemType::LEGENDARY)
     item.update
     expect(item.sell_in).to eql(200)
   end
 
   it 'should not update sell_in when sell_in is 0' do
-    item = Item.new(0, ItemQuality.new(50), ItemType::NORMAL)
+    item = Item.new(0, 50, ItemType::NORMAL)
     item.update
     expect(item.sell_in).to eql(0)
   end
 
   it 'should throw error if not a valid item type' do
-    expect {Item.new(0, ItemQuality.new(50), 'invalid')}.to raise_error('Not a valid item type')
+    expect {Item.new(0, 50, 'invalid')}.to raise_error('Not a valid item type')
+  end
+
+  it 'should not decrease quality' do
+    item = Item.new(0, 50, ItemType::NORMAL)
+    item.update
+    expect(item.quality).to eql(49)
   end
 
   it 'should update decrease quality by 1 when normal' do
-    item = Item.new(0, ItemQuality.new(50), ItemType::NORMAL)
+    item = Item.new(0, 50, ItemType::NORMAL)
     item.update
-    expect(item.item_quality.quality).to eql(49)
+    expect(item.quality).to eql(49)
   end
 end
--- src/item.rb
+++ src/item.rb
@@ -1,17 +1,17 @@
 class Item
-  attr_reader :sell_in, :item_quality, :type
+  attr_reader :sell_in, :quality, :type
 
-  def initialize(sell_in, item_quality, type)
+  def initialize(sell_in, quality, type)
     @sell_in = sell_in
-    @item_quality = item_quality
+    @quality = quality
     @type = type
     raise('Not a valid item type') if ItemType.not_valid_item_type(type)
-    @item_quality.type(type)
   end
+
 
   def update
     update_sell_in
-    @item_quality.update
+    update_quality
   end
 
   private
@@ -19,4 +19,13 @@ class Item
   def update_sell_in
     @sell_in -= 1 if @sell_in.positive? && @type != ItemType::LEGENDARY
   end
+
+  def update_quality
+    update_normal if @type == ItemType::NORMAL
+  end
+
+  def update_normal
+    @quality -= 1 if @quality.positive?
+  end
+
 end
