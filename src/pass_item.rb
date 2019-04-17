7becc480b0b30fb7ff92a184eeef7ad119f8be87
Mahesh Gupta <mahesh@market-pulse.in>
2019-04-17 18:12:08 +0530

    Refactoring

--- src/pass_item.rb
+++ src/pass_item.rb
@@ -1,7 +1,7 @@
 class PassItem < ItemType
 
   def update_sell_in
-    @sell_in -= 1 if @sell_in.positive? && @type != ItemTypeManager::LEGENDARY
+    @sell_in -= 1 if @sell_in.positive?
   end
 
   def update_quality()
